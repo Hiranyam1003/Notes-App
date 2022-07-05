504c2a6fef866861e3dab8b9e3fdd25f4c7215d9
Hiranyam1003 <62459080+Hiranyam1003@users.noreply.github.com>
2022-07-05 19:07:11 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCxDZdQY-h1gBMO-pQGZlw3hNoX3axL93c",
+  authDomain: "notes-app-8e2dd.firebaseapp.com",
+  databaseURL: "https://notes-app-8e2dd-default-rtdb.firebaseio.com",
+  projectId: "notes-app-8e2dd",
+  storageBucket: "notes-app-8e2dd.appspot.com",
+  messagingSenderId: "221261645174",
+  appId: "1:221261645174:web:8cf979cd8089329fd82657"
+};
